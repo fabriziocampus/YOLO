ea23c81080eab5e70fd76ebf5d8d54a2ceb9d319
fabriziocampus <campusfabri@hotmail.it>
2022-10-06 17:24:16 +0200

    Project update

--- main.py
+++ main.py
@@ -29,33 +29,37 @@ def draw_label(input_image, label, left, top):
     cv2.putText(input_image, label, (left, top + dim[1]), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS, cv2.LINE_AA)
 
 
-def pre_process(input_image, net):
+if __name__ == '__main__':
 
-    blob = cv2.dnn.blobFromImage(input_image, 1 / 255, (INPUT_WIDTH, INPUT_HEIGHT), [0, 0, 0], 1, crop=False)
+    classesFile = "YOLOv5/coco.names"
+    classes = None
+    with open(classesFile, 'rt') as f:
+        classes = f.read().rstrip('\n').split('\n')
 
-    net.setInput(blob)
-
-    output_layers = net.getUnconnectedOutLayersNames()
-    outputs = net.forward(output_layers)
-
-    return outputs
-
-
-def post_process(input_image, outputs):
-
+    image = cv2.imread('YOLOv5/image.jpg')
     class_ids = []
     confidences = []
     boxes = []
 
-    rows = outputs[0].shape[1]
+    modelWeights = "YOLOv5/models/yolov5m.onnx"
+    net = cv2.dnn.readNet(modelWeights)
 
-    image_height, image_width = input_image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 1 / 255, (INPUT_WIDTH, INPUT_HEIGHT), [0, 0, 0], 1, crop=False)
 
-    x_factor = image_width / INPUT_WIDTH
-    y_factor = image_height / INPUT_HEIGHT
+    net.setInput(blob)
+
+    output_layers = net.getUnconnectedOutLayersNames()
+    detections = net.forward(output_layers)
+
+    rows = detections[0].shape[1]
+
+    image_height, image_width = image.copy().shape[:2]
+
+    x = image_width / INPUT_WIDTH
+    y = image_height / INPUT_HEIGHT
 
     for r in range(rows):
-        row = outputs[0][0][r]
+        row = detections[0][0][r]
         confidence = row[4]
 
         if confidence >= CONFIDENCE_THRESHOLD:
@@ -63,16 +67,16 @@ def post_process(input_image, outputs):
 
             class_id = np.argmax(classes_scores)
 
-            if (classes_scores[class_id] > SCORE_THRESHOLD):
+            if classes_scores[class_id] > SCORE_THRESHOLD:
                 confidences.append(confidence)
                 class_ids.append(class_id)
 
                 cx, cy, w, h = row[0], row[1], row[2], row[3]
 
-                left = int((cx - w / 2) * x_factor)
-                top = int((cy - h / 2) * y_factor)
-                width = int(w * x_factor)
-                height = int(h * y_factor)
+                left = int((cx - w / 2) * x)
+                top = int((cy - h / 2) * y)
+                width = int(w * x)
+                height = int(h * y)
 
                 box = np.array([left, top, width, height])
                 boxes.append(box)
@@ -84,33 +88,14 @@ def post_process(input_image, outputs):
         top = box[1]
         width = box[2]
         height = box[3]
-        cv2.rectangle(input_image, (left, top), (left + width, top + height), BLUE, 3 * THICKNESS)
+        cv2.rectangle(image, (left, top), (left + width, top + height), BLUE, 3 * THICKNESS)
         label = "{}:{:.2f}".format(classes[class_ids[i]], confidences[i])
-        draw_label(input_image, label, left, top)
-
-    return input_image
-
-
-if __name__ == '__main__':
-
-    classesFile = "YOLOv5/coco.names"
-    classes = None
-    with open(classesFile, 'rt') as f:
-        classes = f.read().rstrip('\n').split('\n')
-
-    frame = cv2.imread('YOLOv5/dog.jpg')
-
-    modelWeights = "YOLOv5/models/yolov5m.onnx"
-    net = cv2.dnn.readNet(modelWeights)
-
-    # Process image.
-    detections = pre_process(frame, net)
-    img = post_process(frame.copy(), detections)
+        draw_label(image, label, left, top)
 
     t, _ = net.getPerfProfile()
     label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
     print(label)
-    cv2.putText(img, label, (20, 40), FONT_FACE, FONT_SCALE, RED, THICKNESS, cv2.LINE_AA)
+    cv2.putText(image, label, (20, 40), FONT_FACE, FONT_SCALE, RED, THICKNESS, cv2.LINE_AA)
 
-    cv2.imshow('Output', img)
+    cv2.imshow('Output', image)
     cv2.waitKey(0)
